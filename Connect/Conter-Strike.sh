5003e1b784bd5ea4be5af9b3d1e37d933cab908c
Drylian <109999325+drylian@users.noreply.github.com>
2023-01-18 17:52:46 -0400

    Update Conter-Strike.sh

--- Connect/Conter-Strike.sh
+++ Connect/Conter-Strike.sh
@@ -17,20 +17,19 @@ if [ "${Arquitetura}" == "arm64" ]; then
    echo "${bold}${lightgreen}="
    echo "${bold}${lightgreen}=============================================================================="
    # Inicio do COdigo
-   if directoryexists(/home/container/steamcmd) then
+   if [[ -f "./steamcmd/steamcmd.sh" ]]; then
       echo "${bold}${lightgreen}==> A Steam Cmd foi detectada, Download não será necessario."
-      /home/container/steamcmd/steamcmd.sh +force_install_dir /home/container/ +login anonymous +app_update 90 +app_set_config 90 mod cstrike +quit
+      ./steamcmd/steamcmd.sh +force_install_dir /home/container/ +login anonymous +app_update 90 +app_set_config 90 mod cstrike +quit
    else
       echo "${bold}${lightgreen}==> A Steam Cmd${bold}${vermelho}Não Detectada${bold}${lightgreen}, O Sistema de download será iniciado."
       curl -sSL -o steamcmd.tar.gz http://media.steampowered.com/installer/steamcmd_linux.tar.gz
       "${bold}${lightgreen}==> O Download foi Terminado Criando Diretorio Steamcmd."
-      mkdir /home/container/steamcmd
+      mkdir ./steamcmd
       "${bold}${lightgreen}Copiando e deletando arquivos desnecessarios."
-      cp steamcmd.tar.gz /home/container/steamcmd/
-      rm -r steamcmd.tar.gz
+      cp steamcmd.tar.gz ./steamcmd/
+      rm -r ./steamcmd.tar.gz
       "${bold}${lightgreen}Iniciando extração de arquivos."
-      cd /home/container/steamcmd/
-      tar -xzvf steamcmd.tar.gz
+      tar -xzvf ./steamcmd/steamcmd.tar.gz
       "${bold}${lightgreen}Extração de arquivos Terminada."
    fi
 done
