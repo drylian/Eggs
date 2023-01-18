08553507fcbdc308c89b316fcc7111368047e3a3
Drylian <109999325+drylian@users.noreply.github.com>
2023-01-18 16:57:21 -0400

    Update Conter-Strike.sh

--- Connect/Conter-Strike.sh
+++ Connect/Conter-Strike.sh
@@ -19,11 +19,11 @@ if [ "${Arquitetura}" == "arm64" ]; then
    echo "${bold}${lightgreen}=============================================================================="
    # Inicio do COdigo
    curl -sSL -o steamcmd.tar.gz http://media.steampowered.com/installer/steamcmd_linux.tar.gz
-   mkdir -p /home/container/steamcmd
-   tar -xzvf steamcmd.tar.gz -C /home/container/steamcmd
+   mkdir -p ./steamcmd
+   tar -xzvf steamcmd.tar.gz -C ./steamcmd
    echo "${bold}${lightgreen}Teste de egg"
-   ./steamcmd/steamcmd.sh +force_install_dir /home/container/ +login anonymous +app_update 90 +app_set_config 90 mod cstrike +quit
-   mkdir -p /home/container/.steam/sdk32
+   ./steamcmd/steamcmd.sh +force_install_dir ./ +login anonymous +app_update 90 +app_set_config 90 mod cstrike +quit
+   mkdir -p ./.steam/sdk32
    cp -v linux32/steamclient.so ../.steam/sdk32/steamclient.so
    ## install end
 echo "-----------------------------------------"
