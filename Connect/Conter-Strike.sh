3a58ce2a99dcaccc2345a0bc128b81098ab1d267
Drylian <109999325+drylian@users.noreply.github.com>
2023-01-18 16:48:06 -0400

    Create Conter-Strike.sh

---
+++ Connect/Conter-Strike.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+Arquitetura=$([ "$(uname -m)" == "x86_64" ] && echo "amd64" || echo "arm64")
+# Cores do Sistema 
+bold=$(echo -en "\e[1m")
+lightgreen=$(echo -en "\e[92m")
+vermelho=$(echo -en "\e[31m")
+# ${bold}${vermelho}
+   echo "${bold}${lightgreen}=============================================================================="
+   echo "${bold}${lightgreen}==>                      Sistema De Suporte Iniciado.                      <=="
+   echo "${bold}${lightgreen}==>                                                                        <=="
+# if do samp03svr
+if [ "${Arquitetura}" == "arm64" ]; then 
+      echo -n "${bold}${lightgreen}==> Arquitetura :Arm64x                                              <="
+   else
+      echo -n "${bold}${lightgreen}==> Arquitetura :AMD64x                                              <="
+   fi
+   echo "${bold}${lightgreen}="
+   echo "${bold}${lightgreen}=============================================================================="
+   # Inicio do COdigo
+   echo "${bold}${lightgreen}Teste de egg"
+   ./steamcmd/steamcmd.sh +force_install_dir /mnt/server +login anonymous +app_update 90 +app_set_config 90 mod cstrike +quit
