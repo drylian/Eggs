2905968970303d1f9c18f90a04c984b9ea0c1b17
Drylian <109999325+drylian@users.noreply.github.com>
2023-01-18 17:58:20 -0400

    Update Conter-Strike.sh

--- Connect/Conter-Strike.sh
+++ Connect/Conter-Strike.sh
@@ -21,7 +21,7 @@ if [ "${Arquitetura}" == "arm64" ]; then
       echo "${bold}${lightgreen}==> A Steam Cmd foi detectada, Download não será necessario."
       ./steamcmd/steamcmd.sh +force_install_dir /home/container/ +login anonymous +app_update 90 +app_set_config 90 mod cstrike +quit
    else
-      echo "${bold}${lightgreen}==> A Steam Cmd${bold}${vermelho}Não Detectada${bold}${lightgreen}, O Sistema de download será iniciado."
+      echo "${bold}${lightgreen}==> A Steam Cmd ${bold}${vermelho}Não Detectada${bold}${lightgreen}, O Sistema de download será iniciado."
       curl -sSL -o steamcmd.tar.gz http://media.steampowered.com/installer/steamcmd_linux.tar.gz
       "${bold}${lightgreen}==> O Download foi Terminado Criando Diretorio Steamcmd."
       mkdir ./steamcmd
@@ -29,7 +29,7 @@ if [ "${Arquitetura}" == "arm64" ]; then
       cp steamcmd.tar.gz ./steamcmd/
       rm -r ./steamcmd.tar.gz
       "${bold}${lightgreen}Iniciando extração de arquivos."
-      tar -xzvf ./steamcmd/steamcmd.tar.gz
+      tar -xzvf ./steamcmd/steamcmd.tar.gz ./steamcmd/
       "${bold}${lightgreen}Extração de arquivos Terminada."
    fi
 done
