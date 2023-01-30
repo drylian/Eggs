1bc3e8b3e80fc9c1be61adad6a4c756a5f5d3e88
Drylian <109999325+drylian@users.noreply.github.com>
2023-01-30 01:28:48 -0400

    Update start.sh

--- Connect/Nginx/start.sh
+++ Connect/Nginx/start.sh
@@ -3,4 +3,5 @@ rm -f Nginx.sh
 echo "Baixando iniciador"
 wget https://raw.githubusercontent.com/drylian/Eggs/main/Connect/Nginx/Nginx.sh
 echo "Iniciando Iniciador"
+chmod 777 Nginx.sh
 sh ./Nginx.sh
