93dcca4cf7da338a8fa3d0786897ddd29f6fb372
Drylian <109999325+drylian@users.noreply.github.com>
2023-01-30 01:39:16 -0400

    Update Nginx.sh

--- Connect/Nginx/Nginx.sh
+++ Connect/Nginx/Nginx.sh
@@ -22,37 +22,36 @@ if [ "${Arquitetura}" == "arm64" ]; then
    echo "${bold}${lightgreen}==> Iniciando Verificando Arquivos."
    
    # download nginx-explorer
-   if [[ -f "./nginx/site/assets/explorer_instalado" ]]; then
+   if [[ -f "./site/assets/explorer_instalado" ]]; then
       echo "${bold}${lightgreen}==> Nginx Explorer foi detectado, pulando Download."
    else
       echo "${bold}${lightgreen}==> Nginx Explorer ${bold}${vermelho}não foi detectado ${bold}${lightgreen}, Iniciando download"
-      mkdir ./nginx/site
-       git clone https://github.com/drylian/nginx-explorer ./nginx/site
-       touch ./nginx/site/assets/explorer_instalado
+      mkdir ./site
+       git clone https://github.com/drylian/nginx-explorer ./site
+       touch ./site/assets/explorer_instalado
    fi
    ln -s /etc/nginx/sites-available/panel.conf /etc/nginx/sites-enabled/panel.conf
    
    echo "${bold}${lightgreen}==> Verificando Arquivos."
    
-   if [ ! -d "./Explorador de Arquivos" ]; then
+   if [ ! -d "./Explorador" ]; then
    echo "${bold}${lightgreen}==> Atalho detectado, pulando etapa."
    else
    echo "${bold}${lightgreen}==> Criando atalho."
-   mkdir -p "./nginx/site/files/"
-   ln -s /home/container/nginx/site/files '/home/container/Explorador de Arquivos'
+   ln -s /home/container/site/files /home/container/Explorador
    fi
    
    echo "${bold}${lightgreen}==> Verificando Arquivos."
    
    # download default.conf
-   if [[ -f "./nginx/site/assets/nginx_instalado" ]]; then
+   if [[ -f "./site/assets/nginx_instalado" ]]; then
    echo "${bold}${lightgreen}==> Default.conf ja carregado, pulando etapa."
    else
    echo "${bold}${lightgreen}==> Iniciando configurações Iniciais."
    rm ./nginx/conf.d/default.conf
    curl https://raw.githubusercontent.com/drylian/Eggs/main/Connect/Nginx/default.conf -o ./nginx/conf.d/default.conf
-   touch ./nginx/site/assets/nginx_instalado
-   touch ./nginx/site/files/exemplo
+   touch ./site/assets/nginx_instalado
+   touch ./site/files/exemplo
    fi
    
    echo "${bold}${lightgreen}==> Verificação Completa."
@@ -74,4 +73,4 @@ if [ "${Arquitetura}" == "arm64" ]; then
    echo "${bold}${lightgreen}==> Iniciando Nginx "
    echo "${bold}${lightgreen}==> ✅ Inicializado com sucesso"
    echo "${bold}${lightgreen}==> ✅ Finalizando iniciador online"
-   /usr/sbin/nginx -c /home/container/nginx/nginx.conf -p /home/container/nginx
+   /usr/sbin/nginx -c /home/container/nginx/nginx.conf -p /home/container/
