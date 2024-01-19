c5cc90cf27021097c8947f1223fd14ecf1d92a2e
Drylian <109999325+drylian@users.noreply.github.com>
2024-01-19 15:55:24 -0300

    Update server.sh

--- Connect/MTA/server.sh
+++ Connect/MTA/server.sh
@@ -5,11 +5,16 @@ if [ "${EXTERNAL_RESOURCES}" ]; then
 
     # Executa o comando ping e verifica o status
     if ping -c 1 "${resource_url}" &>/dev/null; then
-        echo "✅   Conexão com o recurso externo bem-sucedida."
+        echo "✅   Conexão com o recurso externo bem-sucedida. instalando resources"
+        find mods -type f -exec sed -i "s|<httpdownloadurl>\\(.*\\)</httpdownloadurl>|<httpdownloadurl>${EXTERNAL_RESOURCES}</httpdownloadurl>|g" {} +
     else
-        echo "🔴   Falha na conexão com o recurso externo, verifique o servidor do mta external ou remova ele das configurações para inciar o servidor."
+        echo "🔴   Falha na conexão com o recurso externo, verifique o servidor do mta external ou remova ele das configurações para iniciar o servidor."
         exit 0
     fi
+else
+    echo "🟡   Resource Externa está desabilitada, configurando para resource interna."
+
+    find mods -type f -exec sed -i 's|<httpdownloadurl>\(.*\)</httpdownloadurl>|<httpdownloadurl></httpdownloadurl>|g' {} +
 fi
 
 ./mta-server64 --maxplayers ${MAX_PLAYERS} --port ${SERVER_PORT} --httpport ${SERVER_WEBPORT} -n
