1f3fa8715b9db5e080b52479182ae7f7ae186a2c
Drylian <109999325+drylian@users.noreply.github.com>
2024-02-09 23:38:40 -0300

    att

--- Connect/MTA/server.sh
+++ Connect/MTA/server.sh
@@ -16,12 +16,13 @@ else
     # Baixando o httpserver se não estiver presente
     curl -L -o /home/container/mta-accelerator "https://github.com/drylian/Eggs/raw/main/Connect/MTA/Accelerator-Application/build/mta-accelerator"
 fi
+
 chmod 777 mta-accelerator
 
+# Update Server Name
 if [ -z "${SERVER_NAME}" ]; then
     SERVER_NAME="Default MTA Server"
 fi
-# Update Server Name
 find mods -type f -exec sed -i "s|<servername>\\(.*\\)</servername>|<servername>${SERVER_NAME}</servername>|g" {} +
 
 nohup ./mta-accelerator --trace-warnings true --express ${EXPRESS_PORT} >accelerator.log 2>accelerator_error.log &
