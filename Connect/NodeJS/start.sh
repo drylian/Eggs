eda2be388f4bbd5c89fee9478e5eb1be9ebc243c
Drylian <109999325+drylian@users.noreply.github.com>
2024-07-09 07:53:13 -0300

    Update start.sh

--- Connect/NodeJS/start.sh
+++ Connect/NodeJS/start.sh
@@ -4,25 +4,25 @@ if [[ -f "./package.json" ]]; then
     npm install
 fi
 
-if [ "${NODE_MODE}" == "Node JS" ]; then
-    echo "Iniciando node com js"
+if [ "$NODE_MODE" == "Node JS" ]; then
+    echo "Iniciando node com js node ${MAIN_FILE} -- ${NODE_ARGS}"
     node ${MAIN_FILE} -- ${NODE_ARGS}
-else if [ "${NODE_MODE}" == "Node TS" ]; then
+elif [ "${NODE_MODE}" == "Node TS" ]; then
     echo "Iniciando node com ts, verificando se é possivel usar o ts-node"
-    if ![[ -f "./package.json" ]]; then
-      echo "Para usar Typescript no node é necessario o ts-node , baixado no package.json, que não pode ser encontrado em seu sistema"
-      exit
+    if ! [[ -f "./package.json" ]]; then
+        echo "Para usar Typescript no node é necessario o ts-node, baixado no package.json, que não pode ser encontrado em seu sistema"
+        exit
     fi
     echo "Baixando ts-node por padrão"
     npm i ts-node
-    echo "Iniciando typescript com ts-node"
+    echo "Iniciando typescript com ts-node ${MAIN_FILE} -- ${NODE_ARGS}"
     npx ts-node ${MAIN_FILE} -- ${NODE_ARGS}
-else 
+else
     echo "Iniciando usando scripts da package"
-    if ![[ -f "./package.json" ]]; then
-      echo "Para usar com npm scripts é necessario ter uma package.json!"
-      exit
+    if ! [[ -f "./package.json" ]]; then
+        echo "Para usar com npm scripts é necessario ter uma package.json!"
+        exit
     fi
-    echo "Iniciando usando scripts"
+    echo "Iniciando usando o script ${MAIN_FILE}"
     npm run ${MAIN_FILE} -- ${NODE_ARGS}
 fi
